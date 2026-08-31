683f0a78bc2f2f2fa5a67105b832cd9c88c15c88
luna <luna@wvvw.me>
2026-08-31 12:24:07 +0100

    test(sentinel): pin official client contracts
    Refs #55

--- conformance/expo-fixtures/react-native/index.js
+++ conformance/expo-fixtures/react-native/index.js
@@ -1,2 +1,5 @@
 export const AppState = { addEventListener: () => ({ remove() {} }) };
+export const Dimensions = { get: () => ({ width: 390, height: 844, fontScale: 1 }) };
+export const InteractionManager = { runAfterInteractions: (callback) => callback() };
+export const PixelRatio = { get: () => 3 };
 export const Platform = { OS: "android" };
